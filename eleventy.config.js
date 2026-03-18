c604fbd627b27a6b899bcd4bac6948924a27e0a6
enricocovili <henrycovili2004@gmail.com>
2026-03-18 15:01:06 +0100

    moved from articles to news and generated first news page structure

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,8 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { I18nPlugin } from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
-export default async function(eleventyConfig) {
-	
+export default async function (eleventyConfig) {
+
 	// Image Transform works independently, takes <img> on html and transforms automatically
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		extensions: "html",
@@ -26,36 +26,28 @@ export default async function(eleventyConfig) {
 	// RSS: one feed per language
 	eleventyConfig.addPlugin(feedPlugin, {
 		type: "rss",
-		outputPath: "/en/articles/feed.xml",
-		collection: { name: "articles_en", limit: 0 },
+		outputPath: "/en/news/feed.xml",
+		collection: { name: "news_en", limit: 0 },
 		metadata: {
-		language: "en",
-		title: "PesMed News (EN)",
-		subtitle: "Latest news and events from PesMed.",
-		base: "https://pesmed.lorenzostudio.info/",
-		author: { name: "PesMed" }
+			language: "en",
+			title: "PesMed News (EN)",
+			subtitle: "Latest news and events from PesMed.",
+			base: "https://pesmed.lorenzostudio.info/",
+			author: { name: "PesMed" }
 		}
 	});
 
 	eleventyConfig.addPlugin(feedPlugin, {
 		type: "rss",
-		outputPath: "/it/articles/feed.xml",
-		collection: { name: "articles_it", limit: 0 },
+		outputPath: "/it/news/feed.xml",
+		collection: { name: "news_it", limit: 0 },
 		metadata: {
-		language: "it",
-		title: "PesMed News (IT)",
-		subtitle: "Ultime notizie ed eventi da PesMed.",
-		base: "https://pesmed.lorenzostudio.info/",
-		author: { name: "PesMed" }
+			language: "it",
+			title: "PesMed News (IT)",
+			subtitle: "Ultime notizie ed eventi da PesMed.",
+			base: "https://pesmed.lorenzostudio.info/",
+			author: { name: "PesMed" }
 		}
-	});
-
-	eleventyConfig.addCollection("articles_en", (collectionApi) => {
-		return collectionApi.getFilteredByGlob("src/en/articles/*.md").sort((a, b) => b.date - a.date);
-	});
-
-	eleventyConfig.addCollection("articles_it", (collectionApi) => {
-		return collectionApi.getFilteredByGlob("src/it/articles/*.md").sort((a, b) => b.date - a.date);
 	});
 
 	// Nunjucks filter: head (take first N items from array)
@@ -88,10 +80,10 @@ export default async function(eleventyConfig) {
 
 	return {
 		dir: {
-		input: "src",
-		output: "_site",
-		includes: "_includes",
-		data: "_data"
+			input: "src",
+			output: "_site",
+			includes: "_includes",
+			data: "_data"
 		},
 		templateFormats: ["njk", "md"]
 	};
